b29627a218d758b5dc5719f41a456fe08c11f27e
AustinOnshore <81576463+AustinOnshore@users.noreply.github.com>
2021-04-09 22:52:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Shark.PdfConvert;
+
+namespace _4_9_21_Afternoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Attempting to create a pdf using Shark.PdfConvert");
+
+            Console.WriteLine("\n What is the name for your pdf?");
+
+            string fileName = Console.ReadLine();
+
+            string filePath = @"c:\temp\" + fileName + ".pdf";
+
+            Console.WriteLine("Enter the text for the paragraph tag");
+
+            string paragraphText = Console.ReadLine();
+
+            Console.WriteLine("Enter the text for the h1 tag");
+
+            string h1Text = Console.ReadLine();
+
+
+            string content = @"!DOCTYPE html><html><body><h1>" + h1Text + @"</h1><p>" + paragraphText + @"</p></body></html>";
+
+
+            Shark.PdfConvert.PdfConvert.Convert(new PdfConversionSettings
+            {
+
+                Title = "My First Time Using Shark.PdfConvert",
+                Content = content,
+                OutputPath = filePath,
+
+            });
+
+            Console.WriteLine("Converted successfully. File is at: " + filePath);
+
+            Console.ReadLine();
+
+        }
+    }
+}
